0dd9ccdd1c1fa906156a11e3bd48180d5832ab5a
Diluksharandimal <139672294+Diluksharandimal@users.noreply.github.com>
2024-11-09 17:57:13 +0530

    edwdwd

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ const app = express();
 
 // CORS Setup to allow frontend requests
 app.use(cors({
-    origin: 'https://secure-loging-host-client.vercel.app', // Replace with your frontend URL
-    methods: ['GET', 'POST'], // Adjust as necessary
-    allowedHeaders: ['Content-Type'] // Adjust headers as necessary
+    origin: '*', 
   }));
 
 // Security headers for added protection
